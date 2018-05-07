b0e8545be82eaf78e0eea2b2e45be0a117096524
Lisa Mac <lisamac421@gmail.com>
2018-05-07 23:11:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  var newObject =  Object.assign({}, object, { [key]: value })
+  return newObject
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+ object[key] = value 
+ return object
+}
+   
+
