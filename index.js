b085e4773dee4bc49845fa34a061f3d37d568e32
Lisa Mac <lisamac421@gmail.com>
2018-05-07 23:32:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function deleteFromObjectByKey(object, key){
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
-  object = delete object.key
+  object = delete object[key]
   return object
 }
 
